c9b8a73c579313e1d6f07e38f2877087ed1e403f
maryam ayman <maryamayman155@gmail.com>
2026-05-04 02:17:05 +0300

    Initial commit with project structure

---
+++ app/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+from app.database import Base, engine
+from app import models   
+
+
+
+Base.metadata.create_all(bind=engine)
+app = FastAPI(title="Student Management System")
+
+
+@app.get("/")
+def home():
+    return {"message": "Student Management System API is running"}
+
